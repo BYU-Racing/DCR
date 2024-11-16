cc694532eaa638a2f9760600b9ec52641d43f7bb
CZandt <95643051+CZandt@users.noreply.github.com>
2024-11-16 15:56:09 -0700

    Initial With RVC

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <FlexCAN_T4.h>
+#include <DataCollector.h>
+#include <RVC.h>
 
-// put function declarations here:
-int myFunction(int, int);
+constexpr bool ACCEL_CRIT = false;
+constexpr uint32_t ACCEL_INTERVAL = 20;
+
+Adafruit_BNO08x_RVC rvc = Adafruit_BNO08x_RVC();
+RVC accelerometer = RVC(ReservedIDs::RVCId, ACCEL_CRIT, ACCEL_INTERVAL, &rvc);
+
+
+constexpr size_t NUM_SENSORS = 1;
+
+Sensor* SENSORS[] = {
+    &accelerometer
+};
+
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> comsCAN;
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> motorCAN;
+
+DataCollector dcr = DataCollector(ReservedIDs::DCRId, NUM_SENSORS, SENSORS, false);
+
+constexpr uint32_t CAN_BAUD_RATE = 250000;
+constexpr uint32_t SERIAL_BAUD_RATE = 9600;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(SERIAL_BAUD_RATE);
+    comsCAN.begin();
+    comsCAN.setBaudRate(CAN_BAUD_RATE);
+    motorCAN.begin();
+    motorCAN.setBaudRate(CAN_BAUD_RATE);
+
+    Serial1.begin(115200);
+    accelerometer.begin(&Serial1);
+    
+    dcr.begin(&motorCAN, &comsCAN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    dcr.checkSensors();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
